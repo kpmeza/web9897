6540630e5edfe1cf494a3bf3f0420ccad40605b5
kpmeza <kpmeza70@misena.edu.co>
2023-06-20 07:30:09 -0500

    first commit

---
+++ procesos.js
@@ -0,0 +1,24 @@
+function mostrar2() {
+    document.getElementById("conte").innerHTML = "<h1>Hola ficha 2669897</h1>"
+    +"<form action='/action_page.php'>"
+    +"<label for='fname'>First Name</label>"
+    +"<input type='text' id='fname' name='firstname' placeholder='Your name..'>"
+    +"<label for='fname'>First Name</label>"
+    +"<input type='text' id='fname' name='firstname' placeholder='Your name..'>"
+    +"<label for='fname'>First Name</label>"
+    +"<input type='text' id='fname' name='firstname' placeholder='Your name..'>"
+    +" <input type='submit' value='Submit'>"
+    +"</form>";
+}
+function moscar() {
+    document.getElementById("conte").innerHTML = "<h1>Hola ficha 2669897</h1>"
+    +"<form action='/action_page.php'>"
+    +"<label for='fname'>First Name</label>"
+    +"<input type='text' id='fname' name='firstname' placeholder='mostrar carros'>"
+    +" <input type='submit' value='Guardar carros'>"
+    +"</form>";
+}
+function info() {
+    document.getElementById("carga").innerHTML = "<img src='eder olivew.png' alt='' width='100px' >";
+    
+}
